c18095b62df1b2601073a07d496615dbad499dcc
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-28 13:43:42 -0300

    feat: estilizando componentes

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import Container from './components/container/Container'
 import Footer from "./components/footer/footer";
 
 import './app.css'
+import {useState} from "react";
 
 function App() {
   return (
--- src/components/clip/clip.jsx
+++ src/components/clip/clip.jsx
@@ -1,14 +1,32 @@
 import PropTypes from "prop-types";
-import './clip.css';
 import ChooseButton from "../chooseButton/chooseButton";
+import './clip.css';
+import {useRef, useState} from "react";
 
 const Clip = ({data}) => {
+  // use ref (Useref) para manipular o iframe
+  const iframe = useRef(null);
+  const title = useRef(null)
+  const [maxWidth, setMaxWidth] = useState('initial');
+
+  useState(() => {
+    // Quando o componente for montado, adicionar um event listener para o resize que deve atualizar o maxWidth do título para que nunca tenha um texto que ocupe mais de 100% do iframe, quebrando o layout
+    window.addEventListener('resize', () => {
+      if (iframe.current) {
+        // Obter o width do iframe
+        const width = iframe.current.offsetWidth;
+        if (title) setMaxWidth(`${width}px`);
+      }
+    })
+  }, [])
+
   return (
     <div className="clip-info">
-      <h2 className="clip-title">TITULO DO CLIPE <br/> PODE TER 2 LINHAS</h2>
+      <h2 className="clip-title" ref={title} style={{maxWidth: maxWidth}} title={'Título do Clipe'}>TITULO DO CLIPE</h2>
       <div className="clip-username">USERNAME</div>
       <div>
         <iframe
+          ref={iframe}
           className={'clip'}
           src="https://clips.twitch.tv/embed?clip=CoweringColdBaconPipeHype-m6MOE2Woe70ddxw9&parent=localhost"
           allowFullScreen>
