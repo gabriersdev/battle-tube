e3c57e350d609dedcc5ed05d1bc3f263efd5b8b4
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-03-03 21:54:44 -0300

    feat: botao de informar erro removido da pagina de clipe escolhido

--- src/components/chosenClip/ChosenClip.jsx
+++ src/components/chosenClip/ChosenClip.jsx
@@ -74,18 +74,6 @@ const ChosenClip = ({data}) => {
             </span>
               <span>RECOMEÇAR</span>
             </Button>
-            <Button onclick={() => {
-              window.open('https://github.com/eskimozin/battle-tube/issues/new', '_blank', 'noopener,noreferrer')
-            }} classname={'link-external no-margin'}>
-              <span>INFORMAR UM PROBLEMA</span>
-              <span style={{display: 'inline-flex', alignItems: 'center', justifyContent: 'center'}}>
-              <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"
-                   fill="#C6ADFF"
-                   style={{paddingTop: 0, margin: 0, paddingLeft: '0.35rem'}}>
-               <path d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"/>
-              </svg>
-            </span>
-            </Button>
           </div>
           <div className={'chosen-clip-info'}>
             <p className={'chosen-paragraph'}>
