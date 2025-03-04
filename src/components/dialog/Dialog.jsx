53c2fca119a5cfc7d98e801c6b57b905ebb60539
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-03-04 17:10:32 -0300

    feat: componente dialog estilizado; btn de fechar modal adicionado

--- src/components/dialog/Dialog.jsx
+++ src/components/dialog/Dialog.jsx
@@ -14,8 +14,11 @@ const Dialog = () => {
   const dialog = useRef(null);
 
   return (
-    <dialog className={"modal-dialog-analytics"} ref={dialog} style={{textTransform: 'uppercase'}}>
-      <h2>Estatísticas de Clipes do Canal</h2>
+    <dialog className={"modal-dialog-analytics"} ref={dialog}>
+      <hgroup style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap', gap: '1rem'}}>
+        <h2 style={{textWrap: 'balance'}}>Estatísticas de Clipes do Canal</h2>
+        <button className={'btn-close-modal'} style={{margin: 0, padding: '0.25rem 0.5rem', border: '1px solid #C6ADFF50', borderRadius: '5px'}} onClick={(e) => e.target.closest('dialog').close()}>X</button>
+      </hgroup>
       <section style={{textAlign: 'left'}}>
         <div className={'modal-group'}>
           <h3 className={'modal-group-title'}>0S MESES QUE TIVERAM MAIS CLIPES</h3>
