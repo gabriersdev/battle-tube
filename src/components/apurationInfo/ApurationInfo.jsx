5a25e0637f8ca08c8faa7cbe175a8fec2a174a93
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-23 10:08:10 -0300

    feat: animacao alterada

--- src/components/apurationInfo/ApurationInfo.jsx
+++ src/components/apurationInfo/ApurationInfo.jsx
@@ -1,21 +1,44 @@
 import data from '../../data/scrapping.js';
+import {useEffect, useState} from "react";
+import {motion, AnimatePresence} from 'framer-motion'
+
+const countClipsYear = data.totalClips || 'MUITOS';
+const selectionYear = 2024
+
+const pharses = [
+  <p key={1}>FORAM <span className="neon-text">{countClipsYear.toLocaleString('pt-br')}</span> CLIPES FEITOS EM {selectionYear}</p>,
+  <p key={3}>SELECIONAMOS AQUI APENAS OS <span className="neon-text">64 MELHORES</span></p>,
+  <p key={2}>E VOCÊ TEM <span className="neon-text">A MISSÃO</span> DE ESCOLHER <span className="neon-text">O MELHOR</span></p>,
+]
 
 const ApurationInfo = () => {
-  const countClipsYear = data.totalClips || 'MUITOS';
-  const selectionYear = 2024
-
+  const [showeds, setShoweds] = useState([])
+  
+  useEffect(() => {
+    setShoweds([])
+    
+    pharses.forEach((_, index) => {
+      setTimeout(() => {
+        setShoweds(prev => [...prev, !prev.includes(index) ? index : null])
+      }, 1500 * index)
+    })
+  }, [])
+  
   return (
-    <div style={{textAlign: 'center', display: 'flex', gap: '0.5rem', flexDirection: 'column'}}>
-      <div data-aos="fade" data-aos-delay={500} data-aos-duration={500}>
-        <p>FORAM <span className={'neon-text'}>{countClipsYear.toLocaleString('pt-br')}</span> CLIPES FEITOS
-          EM {selectionYear}</p>
-      </div>
-      <div data-aos="fade" data-aos-delay={1500} data-aos-duration={500}>
-        <p>SELECIONAMOS AQUI APENAS OS <span className={'neon-text'}>64 MELHORES</span></p>
-      </div>
-      <div data-aos="fade" data-aos-delay={2500} data-aos-duration={500}>
-        <p>E VOCÊ TEM <span className={'neon-text'}>A MISSÃO</span> DE ESCOLHER O MELHOR</p>
-      </div>
+    <div style={{textAlign: 'center', display: 'flex', gap: '0.25rem', flexDirection: 'column', paddingBottom: '1rem'}}>
+      <AnimatePresence>
+        {showeds.map((i) => (
+          <motion.div
+            key={i}
+            initial={{opacity: 0, y: 10}}
+            animate={{opacity: 1, y: 0}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.5}}
+          >
+            {i || i === 0 ? pharses[i] : null}
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
 }
